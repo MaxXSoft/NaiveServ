88da9cc543766e9a9ccc0b4019e3a8b3d7ba50ae
MaxXing <x@maxxsoft.net>
2019-04-08 17:59:27 +0800

    (version) added header 'version.h'

---
+++ src/util/version.h
@@ -0,0 +1,26 @@
+#ifndef NAIVESERV_UTIL_VERSION_H_
+#define NAIVESERV_UTIL_VERSION_H_
+
+// version information of NaiveServ
+
+#ifndef APP_NAME
+#define APP_NAME            "NaiveServ"
+#endif
+
+#ifndef APP_VERSION
+#define APP_VERSION         "0.0.0"
+#endif
+
+#ifndef APP_VERSION_MAJOR
+#define APP_VERSION_MAJOR   0
+#endif
+
+#ifndef APP_VERSION_MINOR
+#define APP_VERSION_MINOR   0
+#endif
+
+#ifndef APP_VERSION_PATCH
+#define APP_VERSION_PATCH   0
+#endif
+
+#endif // NAIVESERV_UTIL_VERSION_H_
