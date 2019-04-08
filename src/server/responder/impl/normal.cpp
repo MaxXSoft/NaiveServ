40df6b072df07dae4ef7df634d8c6303245e5d00
MaxXing <x@maxxsoft.net>
2019-04-08 23:45:02 +0800

    (normal) completed class 'NormalResponder'

---
+++ src/server/responder/impl/normal.cpp
@@ -0,0 +1,81 @@
+#include <server/responder/impl/normal.h>
+
+#include <map>
+#include <fstream>
+#include <vector>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+
+#include <config/config.h>
+
+namespace {
+
+static const std::map<std::string, std::string> kTypeMap = {
+    {"html", "text/html"},
+    {"htm", "text/html"},
+    {"ico", "image/x-icon"},
+    {"jpeg", "image/jpeg"},
+    {"jpg", "image/jpeg"},
+    {"gif", "image/gif"},
+    {"png", "image/png"},
+};
+
+inline const char *GetTypeByExt(const std::string &ext) {
+    auto it = kTypeMap.find(ext);
+    if (it != kTypeMap.end()) {
+        return it->second.c_str();
+    }
+    else {
+        return "application/octet-stream";
+    }
+}
+
+inline std::string GetFileExtension(const std::string &file) {
+    auto dot_pos = file.rfind('.');
+    if (dot_pos == std::string::npos) return "";
+    return file.substr(dot_pos + 1);
+}
+
+inline std::size_t GetFileSize(std::ifstream &ifs) {
+    auto pos = ifs.tellg();
+    ifs.seekg(0, std::ios::end);
+    auto size = ifs.tellg();
+    ifs.seekg(pos);
+    return size;
+}
+
+} // namespace
+
+HTTPResponse NormalResponder::AcceptRequest(
+        const HTTPParser &parser) const {
+    // check method
+    if (parser.method() != HTTPParser::Method::Get) {
+        // not implemented
+        return HTTPResponse(501);
+    }
+    // get actual url
+    assert(parser.url().find(url_prefix_) != std::string::npos);
+    auto url = parser.url().substr(url_prefix_.size());
+    // get file path
+    auto path = ConfigReader::Instance().www_root();
+    path += url;
+    if (path.back() == '/') path += index_page_;
+    // open file
+    std::ifstream ifs(path, std::ios::binary);
+    if (!ifs.is_open()) {
+        // file not found
+        return HTTPResponse(404);
+    }
+    // get content type
+    auto type = GetTypeByExt(GetFileExtension(url));
+    // read file to buffer
+    std::vector<std::uint8_t> buffer;
+    buffer.resize(GetFileSize(ifs));
+    ifs.read(reinterpret_cast<char *>(buffer.data()), buffer.size());
+    // initialize response
+    HTTPResponse response(200);
+    response.set_data(buffer);
+    response.set_field("Content-Type", type);
+    return response;
+}
--- src/server/responder/impl/normal.h
+++ src/server/responder/impl/normal.h
@@ -1,14 +1,27 @@
 #ifndef NAIVESERV_SERVER_RESPONDER_IMPL_NORMAL_H_
 #define NAIVESERV_SERVER_RESPONDER_IMPL_NORMAL_H_
 
+#include <string>
+
 #include <server/responder/responder.h>
 
 class NormalResponder : public ResponderInterface {
 public:
-    NormalResponder() {}
+    NormalResponder(const ArgList &args) {
+        if (args.size() >= 1) url_prefix_ = args[0];
+        // read index page
+        if (args.size() >= 2) {
+            index_page_ = args[1];
+        }
+        else {
+            index_page_ = "index.html";
+        }
+    }
+
+    HTTPResponse AcceptRequest(const HTTPParser &parser) const override;
 
 private:
-    //
+    std::string url_prefix_, index_page_;
 };
 
 #endif // NAIVESERV_SERVER_RESPONDER_IMPL_NORMAL_H_
