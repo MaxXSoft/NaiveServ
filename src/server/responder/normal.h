444676edbce07a72853d30090dbf59687575c47e
MaxXing <x@maxxsoft.net>
2019-04-08 09:10:13 +0800

    (normal) added class 'NormalResponder'

---
+++ src/server/responder/normal.h
@@ -0,0 +1,14 @@
+#ifndef NAIVESERV_SERVER_RESPONDER_NORMAL_H_
+#define NAIVESERV_SERVER_RESPONDER_NORMAL_H_
+
+#include <server/responder/responder.h>
+
+class NormalResponder : public ResponderInterface {
+public:
+    NormalResponder() {}
+
+private:
+    //
+};
+
+#endif // NAIVESERV_SERVER_RESPONDER_NORMAL_H_
