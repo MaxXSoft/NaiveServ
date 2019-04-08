9747cb73c355750586dbb20dc78e2fcff963a709
MaxXing <x@maxxsoft.net>
2019-04-08 22:57:09 +0800

    (responder) added some type definitions

--- src/server/responder/responder.h
+++ src/server/responder/responder.h
@@ -2,6 +2,10 @@
 #define NAIVESERV_SERVER_RESPONDER_RESPONDER_H_
 
 #include <memory>
+#include <string>
+#include <vector>
+#include <utility>
+#include <map>
 
 #include <http/parser.h>
 #include <http/response.h>
@@ -13,6 +17,10 @@ public:
     virtual HTTPResponse AcceptRequest(const HTTPParser &parser) const = 0;
 };
 
+// type definitions about responder
 using Responder = std::shared_ptr<ResponderInterface>;
+using ArgList = std::vector<std::string>;
+using ResponderRule = std::pair<std::string, ArgList>;
+using ResponderRules = std::map<std::string, ResponderRule>;
 
 #endif // NAIVESERV_SERVER_RESPONDER_RESPONDER_H_
