c81c43b1132d51d1a75a365b655f13f0776f0b4d
MaxXing <x@maxxsoft.net>
2019-04-09 19:54:54 +0800

    (main) modified some details

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
+#include <cstdlib>
+
+#include <signal.h>
+
 #include <server/core.h>
+#include <daemon/daemon.h>
 
 namespace {
 
-//
+void SignalHandler(int sig) {
+    exit(0);
+}
 
 } // namespace
 
 int main(int argc, const char *argv[]) {
-    auto &core = Core::Instance();
-    core.StartListen();
+    if (!RunDaemon(argc, argv)) {
+        // register signal handler
+        signal(SIGINT, SignalHandler);
+        // run server core
+        auto &core = Core::Instance();
+        core.StartListen();
+    }
     return 0;
 }
