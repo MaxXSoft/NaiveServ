5b9cf3570f9b170c627fe8bb136c423cedfacc36
MaxXing <x@maxxsoft.net>
2019-04-09 13:20:30 +0800

    (table) added more functions

--- src/server/responder/table.h
+++ src/server/responder/table.h
@@ -13,20 +13,30 @@
 
 
 // some other magic macros
-#define RESPONDER_DECLARE(resp) \
-    if (name == #resp) { \
-        return std::make_shared<resp##Responder>(args); \
+#define NEW_RESPONDER(r) \
+    if (name == #r) { \
+        return std::make_shared<r##Responder>(args); \
     }
 
-#define RESPONDER_EXPAND(responders) \
-    responders(RESPONDER_DECLARE)
+#define RESPONDER_NAME_ARRAY(r)     #r,
 
-// get responder
+#define RESPONDER_VALID(r)          if (name == #r) { return true; }
+
+#define RESPONDER_EXPAND(macro, responders) \
+    responders(macro)
+
+// create responder
 inline Responder NewResponder(const std::string &name,
         const ArgList &args) {
-    RESPONDER_EXPAND(ALL_RESPONDERS);
+    RESPONDER_EXPAND(NEW_RESPONDER, ALL_RESPONDERS);
     // default path
     return nullptr;
+}
+
+// check responder name is valid
+inline bool IsResponderValid(const std::string &name) {
+    RESPONDER_EXPAND(RESPONDER_VALID, ALL_RESPONDERS);
+    return false;
 }
 
 #endif // NAIVESERV_SERVER_RESPONDER_TABLE_H_
